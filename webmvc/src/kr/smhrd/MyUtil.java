dea2f7ef02514b420ed8836500a8a7c112adf4b1
Dong Jun <Dong Jun@DESKTOP-NO1871P>
2020-09-07 17:08:15 +0900

    수정정

--- webmvc/src/kr/smhrd/MyUtil.java
+++ webmvc/src/kr/smhrd/MyUtil.java
@@ -4,9 +4,9 @@ public class MyUtil {
    public int hap() {
 	   //누구야 수정좀-->완료했어
 	   int sum = 0;
-	   for(int i=0;i<=100;i++) {
-		   sum += 0;
+	   for(int i=1;i<=100;i++) {
+		   sum += i;
 	   }
-	   return 5;
+	   return sum;
    }
  }
